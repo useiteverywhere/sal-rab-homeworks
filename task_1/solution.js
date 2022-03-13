10db4eb5d0e1bd27fe9f2e25eefdce607f662c95
b3cesh <b3cesh@gmail.com>
2022-03-14 00:31:42 +0300

    task1-part1

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = title + ' ' + 'за' + ' ' + price + ' ' + 'теперь в корзине!';
+
+    console.log(message);
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -18,6 +21,11 @@ function addInCartMessage(title, price) {
 
     return message;
 }
+
+
+
+
+
 
 function addInCartCountChange(value) {
 
