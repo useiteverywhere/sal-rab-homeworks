2bf83783769d4698b511d36bb253a798554f5303
b3cesh <b3cesh@gmail.com>
2022-06-01 21:22:17 +0400

    final-3

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,29 +34,21 @@
 //    }
 // }
  
-function sendRequest(name, phone, address, goods, sum) {
-    
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, address, goods, sum) {  
+    let data = {
+        client: name + " " + phone,
+        order: {
+            address: ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat},
+            sum: sum
+        },
+        goods: []
+    }
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push(goods[i]);
-          }
-
-    let address = `${street} + ${house} + ${entrance} + ${floor} + ${flat}`;
-
-    let data.client = `${name} + ${phone}`;
-
-    
-
-    data.order.address = address;
-    
-    data.order.sum = sum;
-
-    //data.client = ${name} + ' ' + ${phone};
-    //"Иван" + " " + "+7(987)65-43-210";
-    
+    }    
 
     let jsonData = JSON.stringify({data: data});
 
