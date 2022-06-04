550271690f683e3d2ee0e151b623440c8247c04c
b3cesh <b3cesh@gmail.com>
2022-06-04 17:10:23 +0400

    final-5

--- task_final/solution.js
+++ task_final/solution.js
@@ -41,16 +41,22 @@ function sendRequest(name, phone, address, goods, sum) {
             address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
             sum: sum
         },
-        goods: []       ]
+        goods: []
     }
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i < countOfGoods; i += 1) {
-    data.goods.push(goods[i]);
-    }    
 
-     let jsonData = JSON.stringify({data: data});
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let singleGoods = {
+            title: goods[i].title,
+            count: goods[i].count
+        }
+        data.goods.push(singleGoods);
+    }
+
+
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
